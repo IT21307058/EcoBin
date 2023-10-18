35e62ec2ac15ac62a877494622f4654bcf3aae1e
IT21440236 <it21440236@my.sliit.lk>
2023-10-18 07:51:46 +0530

    #yeran-blog navigation

--- App.js
+++ App.js
@@ -1,42 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import React, { useState, useEffect } from 'react';
-import { firebase } from './config'
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import React, { useState, useEffect } from "react";
+import { firebase } from "./config";
 
 const Stack = createNativeStackNavigator();
 const Tab = createMaterialBottomTabNavigator();
 const TopTabs = createMaterialTopTabNavigator();
 
-import colors from "../EcoBin/src/styles/color"
+import colors from "../EcoBin/src/styles/color";
 
-import AdvertiseHome from './src/Screen/Advertise/AdvertiseHome';
-import AddAdvertise from './src/Screen/Advertise/AddAdvertise';
-import UpdateAdvertise from './src/Screen/Advertise/UpdateAdvertise';
+import AdvertiseHome from "./src/Screen/Advertise/AdvertiseHome";
+import AddAdvertise from "./src/Screen/Advertise/AddAdvertise";
+import UpdateAdvertise from "./src/Screen/Advertise/UpdateAdvertise";
 // import OneAdvertise from './src/Screen/Advertise/OneAdvertise';
 
-import AllCommunity from './src/Screen/Community/AllCommunity';
-import AddPost from './src/Screen/Community/AddPost';
-import UpdatePost from './src/Screen/Community/UpdatePost';
-import OnePost from './src/Screen/Community/OnePost';
-import FeedbackPage from './src/Screen/Community/FeedbackPage';
+import AllCommunity from "./src/Screen/Community/AllCommunity";
+import AddPost from "./src/Screen/Community/AddPost";
+import UpdatePost from "./src/Screen/Community/UpdatePost";
+import OnePost from "./src/Screen/Community/OnePost";
+import FeedbackPage from "./src/Screen/Community/FeedbackPage";
 
-import UserAccount from './src/Screen/UserAccount/UserAccount';
-import Home from './src/Screen/Home/Home';
+import UserAccount from "./src/Screen/UserAccount/UserAccount";
+import Home from "./src/Screen/Home/Home";
 
-import imagePath from './src/constants/imagePath';
-import OneAdvertise from './src/Screen/Advertise/OneAdvertise';
-import OnlyProduct from './src/Screen/Advertise/OnlyProduct';
-import OnlyProgram from './src/Screen/Advertise/OnlyProgram';
-import CommunityHome from './src/Screen/Community/CommunityHome';
-import EducationalUserPage from './src/Screen/Community/EducationalUserPage';
-import EnvironmentOrganizationPage from './src/Screen/Community/EnvironmentOrganizationPage';
+import imagePath from "./src/constants/imagePath";
+import OneAdvertise from "./src/Screen/Advertise/OneAdvertise";
+import OnlyProduct from "./src/Screen/Advertise/OnlyProduct";
+import OnlyProgram from "./src/Screen/Advertise/OnlyProgram";
+import CommunityHome from "./src/Screen/Community/CommunityHome";
+import EducationalUserPage from "./src/Screen/Community/EducationalUserPage";
+import EnvironmentOrganizationPage from "./src/Screen/Community/EnvironmentOrganizationPage";
 
-import Login from './src/Screen/Auth/Login';
-import Registeration from './src/Screen/Auth/Registeration';
+import Login from "./src/Screen/Auth/Login";
+import Registeration from "./src/Screen/Auth/Registeration";
+
+import BlogHome from "./src/Screen/Blog/BlogHome"; // Import the BlogHome screen
 
 function TopTabsGroup() {
   return (
@@ -45,11 +47,10 @@ function TopTabsGroup() {
       <TopTabs.Screen name="Product" component={OnlyProduct} />
       <TopTabs.Screen name="Program" component={OnlyProgram} />
     </TopTabs.Navigator>
-  )
+  );
 }
 
 function TabNavigator() {
-
   const [intializing, setInitilizing] = useState(true);
   const [user, setUser] = useState();
 
@@ -68,10 +69,10 @@ function TabNavigator() {
   if (!user) {
     return (
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name='Login' component={Login}  />
-        <Stack.Screen name='Registeration' component={Registeration}/>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Registeration" component={Registeration} />
       </Stack.Navigator>
-    )
+    );
   }
 
   return (
@@ -79,75 +80,109 @@ function TabNavigator() {
       screenOptions={{
         headerShown: false,
         tabBarInactiveTintColor: colors.blackOpacity50,
-        tabBaractiveTintColor: colors.themeColor
+        tabBaractiveTintColor: colors.themeColor,
       }}
     >
-      <Tab.Screen name='Home' component={Home}
+      <Tab.Screen
+        name="Home"
+        component={Home}
         options={{
           tabBarIcon: ({ focused }) => {
             return (
-              <Image style={{
-                tintColor: focused ? colors.themeColor : colors.blackOpacity50
-              }} source={imagePath.blueHome} />
-            )
-          }
+              <Image
+                style={{
+                  tintColor: focused
+                    ? colors.themeColor
+                    : colors.blackOpacity50,
+                }}
+                source={imagePath.blueHome}
+              />
+            );
+          },
         }}
       />
-      <Tab.Screen name='Community' component={StackNavigator}
+      <Tab.Screen
+        name="Community"
+        component={StackNavigator}
         options={{
           tabBarIcon: ({ focused }) => {
             return (
-              <Image style={{
-                tintColor: focused ? colors.themeColor : colors.blackOpacity50
-              }} source={imagePath.community} />
-            )
-          }
+              <Image
+                style={{
+                  tintColor: focused
+                    ? colors.themeColor
+                    : colors.blackOpacity50,
+                }}
+                source={imagePath.community}
+              />
+            );
+          },
         }}
       />
-      <Tab.Screen name='Advertise' component={TopTabsGroup}
+      <Tab.Screen
+        name="Advertise"
+        component={TopTabsGroup}
         options={{
           tabBarIcon: ({ focused }) => {
             return (
-              <Image style={{
-                tintColor: focused ? colors.themeColor : colors.blackOpacity50
-              }} source={imagePath.blog} />
-            )
-          }
+              <Image
+                style={{
+                  tintColor: focused
+                    ? colors.themeColor
+                    : colors.blackOpacity50,
+                }}
+                source={imagePath.blog}
+              />
+            );
+          },
         }}
       />
-      <Tab.Screen name='Profile' component={UserAccount}
+      <Tab.Screen
+        name="Profile"
+        component={UserAccount}
         options={{
           tabBarIcon: ({ focused }) => {
             return (
-              <Image style={{
-                tintColor: focused ? colors.themeColor : colors.blackOpacity50
-              }} source={imagePath.user} />
-            )
-          }
+              <Image
+                style={{
+                  tintColor: focused
+                    ? colors.themeColor
+                    : colors.blackOpacity50,
+                }}
+                source={imagePath.user}
+              />
+            );
+          },
         }}
       />
     </Tab.Navigator>
-  )
+  );
 }
 
 function StackNavigator() {
-
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name='CommunityHome' component={CommunityHome} />
-      <Stack.Screen name='EducationalUserPage' component={EducationalUserPage} />
-      <Stack.Screen name='EnvironmentOrganizationPage' component={EnvironmentOrganizationPage} />
-      <Stack.Screen name='AllCommunity' component={AllCommunity} />
-      <Stack.Screen name='AddPost' component={AddPost} />
-      <Stack.Screen name='UpdatePost' component={UpdatePost} />
-      <Stack.Screen name='OnePost' component={OnePost} />
-      <Stack.Screen name='FeedbackPage' component={FeedbackPage} />
+      <Stack.Screen name="CommunityHome" component={CommunityHome} />
+      <Stack.Screen
+        name="EducationalUserPage"
+        component={EducationalUserPage}
+      />
+      <Stack.Screen
+        name="EnvironmentOrganizationPage"
+        component={EnvironmentOrganizationPage}
+      />
+      <Stack.Screen name="AllCommunity" component={AllCommunity} />
+      <Stack.Screen name="AddPost" component={AddPost} />
+      <Stack.Screen name="UpdatePost" component={UpdatePost} />
+      <Stack.Screen name="OnePost" component={OnePost} />
+      <Stack.Screen name="FeedbackPage" component={FeedbackPage} />
       {/* <Stack.Screen name='AdvertiseHome' component={AdvertiseHome} /> */}
-      <Stack.Screen name='AddAdvertise' component={AddAdvertise} />
-      <Stack.Screen name='UpdateAdvertise' component={UpdateAdvertise} />
-      <Stack.Screen name='OneAdvertise' component={OneAdvertise} />
+      <Stack.Screen name="AddAdvertise" component={AddAdvertise} />
+      <Stack.Screen name="UpdateAdvertise" component={UpdateAdvertise} />
+      <Stack.Screen name="OneAdvertise" component={OneAdvertise} />
+      <Stack.Screen name="BlogHome" component={BlogHome} />
     </Stack.Navigator>
-  )
+  );
 }
 
 export default function App() {
--- src/Screen/Home/Home.js
+++ src/Screen/Home/Home.js
@@ -1,11 +1,27 @@
-import { StyleSheet, Text, View, ImageBackground, SafeAreaView, TextInput, FlatList, Button, Image, TouchableOpacity } from 'react-native'
-import React, { useState, useEffect } from 'react'
-import { scale, verticalScale, moderateScale, moderateVerticalScale } from 'react-native-size-matters';
-import imagePath from '../../constants/imagePath';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  SafeAreaView,
+  TextInput,
+  FlatList,
+  Button,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  scale,
+  verticalScale,
+  moderateScale,
+  moderateVerticalScale,
+} from "react-native-size-matters";
+import imagePath from "../../constants/imagePath";
 // import dummyData from './dummyData';
-import ButtonComp from '../../Components/ButtonComp';
-import colors from '../../styles/color';
-import { useNavigation } from '@react-navigation/native'
+import ButtonComp from "../../Components/ButtonComp";
+import colors from "../../styles/color";
+import { useNavigation } from "@react-navigation/native";
 // import { TouchableOpacity } from 'react-native-gesture-handler';
 //bhanuka
 
@@ -14,10 +30,7 @@ const Home = (props) => {
 
   return (
     <View style={styles.container}>
-      <ImageBackground
-        source={imagePath.background}
-        style={styles.imgStyle}
-      >
+      <ImageBackground source={imagePath.background} style={styles.imgStyle}>
         <SafeAreaView style={{ flex: 1 }}>
           <View style={styles.headerStyle}>
             <Image source={imagePath.backarrow} />
@@ -34,9 +47,12 @@ const Home = (props) => {
             <Image source={imagePath.event} />
             <Text style={styles.cardText}>Events</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.card}>
+          <TouchableOpacity
+            style={styles.card}
+            onPress={() => navigation.navigate("BlogHome")}
+          >
             <Image source={imagePath.goal} />
-            <Text style={styles.cardText}>Goals</Text>
+            <Text style={styles.cardText}>Blogs</Text>
           </TouchableOpacity>
         </View>
         <View style={styles.row}>
@@ -50,12 +66,11 @@ const Home = (props) => {
           </TouchableOpacity>
         </View>
       </View>
-
     </View>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
 
 const styles = StyleSheet.create({
   container: {
@@ -63,39 +78,39 @@ const styles = StyleSheet.create({
   },
   imgStyle: {
     height: 200,
-    width: '100%',
+    width: "100%",
   },
   AdvertiseTextStyle: {
     fontSize: scale(32),
-    color: 'white',
-    fontWeight: 'bold',
-    marginLeft: 145
+    color: "white",
+    fontWeight: "bold",
+    marginLeft: 145,
   },
   headerStyle: {
     paddingVertical: moderateVerticalScale(16),
-    shadowColor: '#000',
+    shadowColor: "#000",
     shadowOffset: { width: 0, height: 6 },
     shadowOpacity: 0.2,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     padding: 16,
-    paddingHorizontal: moderateScale(16)
+    paddingHorizontal: moderateScale(16),
   },
   row: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginBottom: 16
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 16,
   },
   card: {
     flex: 1,
-    backgroundColor: '#f0f0f0',
+    backgroundColor: "#f0f0f0",
     borderRadius: 10,
     padding: 20,
     margin: 8,
-    alignItems: 'center',
-    justifyContent: 'center',
-    shadowColor: '#000',
+    alignItems: "center",
+    justifyContent: "center",
+    shadowColor: "#000",
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.5,
     shadowRadius: 2,
@@ -103,10 +118,10 @@ const styles = StyleSheet.create({
   },
   cardText: {
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
-  cardContainer:{
-    marginTop:50,
-    paddingHorizontal: 40
-  }
-})
+  cardContainer: {
+    marginTop: 50,
+    paddingHorizontal: 40,
+  },
+});
