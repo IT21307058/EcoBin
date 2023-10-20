51865a0262adb31252c0c676e0e5aed629397a3b
IT21440236 <it21440236@my.sliit.lk>
2023-10-20 23:33:50 +0530

    yeran made changes to config.js

--- config.js
+++ config.js
@@ -5,15 +5,6 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
 const firebaseConfig = {
-  // apiKey: "AIzaSyBbyTAUfH7PmxyYOw_VjB8cg-NpIU_R1FE",
-  // authDomain: "realtimeexpo-df4ff.firebaseapp.com",
-  // databaseURL: "https://realtimeexpo-df4ff-default-rtdb.asia-southeast1.firebasedatabase.app/",
-  // projectId: "realtimeexpo-df4ff",
-  // storageBucket: "realtimeexpo-df4ff.appspot.com",
-  // messagingSenderId: "821691245707",
-  // appId: "1:821691245707:web:0fd0c56f3cfe802747a08e",
-  // measurementId: "G-PW8YW4FJH6"
-
   // apiKey: "AIzaSyAbeR2z0c4lyi7ERzy_IcvKQmphOL7H3a0",
   // authDomain: "realtimeexpo2.firebaseapp.com",
   // databaseURL: "https://realtimeexpo2-default-rtdb.asia-southeast1.firebasedatabase.app/",
